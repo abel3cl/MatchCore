229a3b13642720c0abaca70244b0c064817f5950
Abel Castro <abel3cl@gmail.com>
2018-09-17 20:22:16 +0100

    Added action

---
+++ Classes/Action.swift
@@ -0,0 +1,14 @@
+//
+//  Action.swift
+//  Pods
+//
+//  Created by Abel Castro on 17/9/18.
+//
+
+import Foundation
+
+enum Action: String, Codable {
+    case start
+    case left
+    case tuttifruti
+}
